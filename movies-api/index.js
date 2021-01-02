91a2b2b2b74ec884af5291719561be394d30623d
Conor Brett <conorbrett72@gmail.com>
2021-01-02 17:42:19 +0000

    Week 9 Included Mongoose

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,8 +1,11 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
 import bodyParser from 'body-parser';
+import './db';
+import {loadUsers} from './seedData'
 
 dotenv.config();
 
@@ -15,6 +18,9 @@ const errHandler = (err, req, res, next) => {
   res.status(500).send(`Hey!! You caught the error 👍👍, ${err.stack} `);
 };
 
+if (process.env.SEED_DB) {
+  loadUsers();
+}
 
 const app = express();
 
